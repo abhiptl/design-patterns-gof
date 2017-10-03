e94f710e43c5d277ac5e1a98c4a03f6f7bbd2d06
Abhishek Patel <abhishek.patel@sanelib.com>
2017-10-03 17:24:02 +0530

    update welcome message

--- src/com/abhishek/DesignPatternMain.java
+++ src/com/abhishek/DesignPatternMain.java
@@ -3,6 +3,6 @@ package com.abhishek;
 public class DesignPatternMain {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Design patterns !");
+        System.out.println("Welcome to Design patterns !!");
     }
 }
